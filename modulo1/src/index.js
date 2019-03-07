2dd0c4b4a25d4187b8e37918583ef4cb477ce351
camposLuis <luis.campos.henrique@hotmail.com>
2019-03-07 17:17:03 -0300

    Configuração inicial GoNative

---
+++ modulo1/src/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+
+export default class App extends Component {
+  render() {
+    return <View style={styles.container} />;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#333"
+  }
+});
