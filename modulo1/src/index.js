4456aecf0432a843ace1a33491b6228b713abb1d
camposLuis <luis.campos.henrique@hotmail.com>
2019-03-07 18:07:42 -0300

    Estado e imutabilidade

--- modulo1/src/index.js
+++ modulo1/src/index.js
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
-
+import { Platform, StyleSheet, Text, View, Button } from "react-native";
 import Todo from "./components/Todo";
 
 export default class App extends Component {
+  state = {
+    usuario: { id: 0, nome: "Luís" },
+    todos: [
+      { id: 0, text: "Fazer café" },
+      { id: 0, text: "Estudar o GoNative" }
+    ]
+  };
+  addTodo = () => {
+    this.setState({
+      todos: [...this.state.todos, { id: Math.random(), text: "Novo todo" }]
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Todo title="Fazer café" />
-        <Todo title={"Bora dev!!!"} />
+        <Text>{this.state.usuario.nome}</Text>
+        {this.state.todos.map(todo => (
+          <Todo key={todo.id} title={todo.text} />
+        ))}
+        <Button title="Adicionar todo" onPress={this.addTodo} />
       </View>
     );
   }
@@ -19,6 +34,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#AAA"
+    backgroundColor: "#FFF"
   }
 });
