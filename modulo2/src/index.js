f2145e81e1cd4178a591cac6c57d530301a418a8
camposLuis <luis.campos.henrique@hotmail.com>
2019-03-08 16:11:40 -0300

    Navegação

--- modulo2/src/index.js
+++ modulo2/src/index.js
@@ -2,22 +2,8 @@ import './config/ReactotronConfig';
 import './config/DevtoolsConfig';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Routes from './routes';
 
-const bgColorContainer = '#F5FCFF';
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    backgroundColor: bgColorContainer,
-    flex: 1,
-    justifyContent: 'center',
-  },
-});
-
-const App = () => (
-  <View style={styles.container}>
-    <Text>Template App</Text>
-  </View>
-);
+const App = () => <Routes />;
 
 export default App;
