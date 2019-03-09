4beb6646953c8d4468c1b1546670023a20cac91d
camposLuis <luis.campos.henrique@hotmail.com>
2019-03-08 21:38:44 -0300

    Consumindo repos da api

--- modulo2/src/pages/Repositories/index.js
+++ modulo2/src/pages/Repositories/index.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  View, AsyncStorage, Text, ActivityIndicator,
+} from 'react-native';
 import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import api from '../../services/api';
 import Header from '../../components/Header';
 
-// import styles from './styles';
+import styles from './styles';
 // AsyncStorage.clear();
-
-const Repositories = () => (
-  <View>
-    <Header title="Repositórios" />
-  </View>
-);
-
 const TabIcon = ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />;
 
 TabIcon.propTypes = {
   tintColor: PropTypes.string.isRequired,
 };
 
-Repositories.navigationOptions = {
-  tabBarIcon: TabIcon,
-};
+export default class Repositories extends Component {
+  static navigationOptions = {
+    tabBarIcon: TabIcon,
+  };
 
-export default Repositories;
+  state = {
+    data: [],
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const username = await AsyncStorage.getItem('@Githuber:username');
+    const { data } = await api.get(`/users/${username}/repos`);
+
+    this.setState({ data, loading: false });
+  }
+
+  renderList = () => <Text>Lista</Text>;
+
+  render() {
+    const { loading } = this.state;
+    return (
+      <View>
+        <Header title="Repositórios" />
+        {loading ? <ActivityIndicator style={styles.loading} /> : this.renderList()}
+      </View>
+    );
+  }
+}
