37131726d22a61720bf4ee2113163e803cb0a8d5
camposLuis <luis.campos.henrique@hotmail.com>
2019-03-16 09:43:19 -0300

    Refatorando as actions

--- modulo3/src/TodoList.js
+++ modulo3/src/TodoList.js
@@ -2,24 +2,23 @@ import React, { Component } from 'react';
 
 import { View, Text, Button } from 'react-native';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as Actions from './actions/todos';
 
 // import styles from './styles';
 
-const TodoList = ({ todos, dispatch }) => (
+const TodoList = ({ todos, addTodo, markAsCompleted }) => (
   <View style={{ flex: 1, backgroundColor: '#FFF', justifyContent: 'center' }}>
     {todos.map(todo => (
       <Text
-        onPress={() => dispatch({ type: 'MARK_AS_COMPLETED', id: todo.id })}
+        onPress={() => markAsCompleted(todo.id)}
         style={{ textDecorationLine: todo.completed ? 'line-through' : 'none' }}
         key={todo.id}
       >
         {todo.text}
       </Text>
     ))}
-    <Button
-      onPress={() => dispatch({ type: 'ADD_TODO', text: 'Novo Todo' })}
-      title="Adicionar todo"
-    />
+    <Button onPress={addTodo} title="Adicionar todo" />
   </View>
 );
 
@@ -27,4 +26,9 @@ const mapStateToProps = state => ({
   todos: state,
 });
 
-export default connect(mapStateToProps)(TodoList);
+const mapDispatchToProps = dispatch => bindActionCreators(Actions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(TodoList);
