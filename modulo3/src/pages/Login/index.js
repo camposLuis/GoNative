7f355892beee47d25ceae182e79c280c02a66c96
camposLuis <luis.campos.henrique@hotmail.com>
2019-03-16 16:57:13 -0300

    Autenticação

--- modulo3/src/pages/Login/index.js
+++ modulo3/src/pages/Login/index.js
@@ -1,26 +1,39 @@
 import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import api from '../../services/api';
+import * as LoginActions from '../../store/actions/login';
 
 import {
   Container, Input, Button, ButtonText, Error,
 } from './styles';
 
-export default class Login extends Component {
+class Login extends Component {
   state = { username: '' };
 
   handleSubmit = async () => {
     const { username } = this.state;
+    const { loginSuccess, loginFailure, navigation } = this.props;
 
     try {
       await api.get(`/users/${username}`);
-    } catch (err) {}
+
+      loginSuccess(username);
+
+      navigation.navigate('Repositories');
+    } catch (err) {
+      loginFailure();
+    }
   };
 
   render() {
     const { username } = this.state;
+    const { error } = this.props;
 
     return (
       <Container>
+        {error && <Error>Usuário inexistente</Error>}
         <Input
           value={username}
           onChangeText={text => this.setState({ username: text })}
@@ -35,3 +48,14 @@ export default class Login extends Component {
     );
   }
 }
+
+const mapStateToProps = state => ({
+  error: state.login.error,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(LoginActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Login);
